f2c04678a4c9d5ef2212315fcf4bd510b3c4418a
DarkcTime <vlad_chekmarev_01@mail.ru>
2022-09-16 11:41:46 +0300

    add info_item and buy_item def

--- stripe_api/models.py
+++ stripe_api/models.py
@@ -1,3 +1,4 @@
+from unicodedata import decimal
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     price = models.DecimalField(decimal_places=2, max_digits=10000000)
+
+    def get_price_dec(self):
+        price_str = str(self.price).replace('.',',')
+        return decimal(price_str) 
--- stripe_api/views.py
+++ stripe_api/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from stripe_api.models import Item
+import stripe
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def info_item(request, id):
-    pass
+    #if 'buy_item_button' in request.POST:
+    #    return render(request, "index.html", data={"id":id})
+    item = Item.objects.get(id=id)
+    data = {"item": item}
+    return render(request, "item_info.html", data)
+
+stripe.api_key = 'sk_test_51Lhw8lJv44QRunnSBix4VHqjJxGeUZkayk6Xf03SHj6GyRNY1e6SybvPPKSf7AjRT4VgyDUDmJEZr8G09WQMzNIW00vfdt4uwE'
 
 def buy_item(request, id):
-    pass
+    item = Item.objects.get(id=id)
+    session = stripe.checkout.Session.create(
+        line_items=[{
+        'price_data': {
+            'currency': 'usd',
+            'product_data': {
+            'name': item.name,
+            'description': item.description
+            },
+            'unit_amount_decimal': 1000.99 * 100,
+        },
+        'quantity': 1,
+        }],
+        mode='payment',
+        success_url='https://example.com/success',
+        cancel_url='https://example.com/cancel',
+    )
+
+    return redirect(session.url, code=303)
