ae86f966a11fb72ec7218c4f6463c04d314b5664
DarkcTime <vlad_chekmarev_01@mail.ru>
2022-09-16 10:57:23 +0300

    add models

--- stripe_api/models.py
+++ stripe_api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(decimal_places=2, max_digits=10000000)
