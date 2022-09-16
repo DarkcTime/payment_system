76a5ffd71ab57ca4276d7f8a32277814ee13ec78
DarkcTime <vlad_chekmarev_01@mail.ru>
2022-09-16 13:57:41 +0300

    fix for deploy vercel

--- ApiPaymentSystem/settings.py
+++ ApiPaymentSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^zci2tetf#93(0h^ee@8p*k+r(v*r0p7s^8^-ni7@ia+(j(c&@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.ApiPaymentSystem.app']
 
 
 # Application definition
